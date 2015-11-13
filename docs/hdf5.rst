49b7d3f139610467d096e44d494fc34f5860afc3
Mark Carter <mcturra2000@yahoo.co.uk>
2015-11-13 21:57:34 +0000

    Added hdf5 processing

--- docs/hdf5.rst
+++ docs/hdf5.rst
@@ -18,6 +18,16 @@ Download the latest version of the sources, and issue the command::
     ./configure --enable-fortran --prefix=/usr/local
 
 
+CSV to HDF5
+-----------
+
+Issue the command::
+
+  mython hdf --to5 <filename>
+
+This assumes that `filename` is in `~/.fortran` and is in CSV format. It will then create a file  named `filename.h5`. You might then want to aggregate it up into a bigger repository using `h5copy`
+
+
 Fortran
 -------
 
@@ -48,3 +58,10 @@ Download and install either of the following applications:
 
 * `HDF5View <http://www.hdfgroup.org/products/java/release/download.html>`_
 * `ViTables <http://vitables.org/>`_
+
+Module documents
+----------------
+
+.. automodule:: mython.hdf
+   :members:
+      
---
+++ mython/hdf.py
@@ -0,0 +1,50 @@
+"""
+Library for converting to/from HDF5
+"""
+
+import argparse
+import datetime
+import os.path
+
+import h5py
+import numpy as np
+import pandas as pd
+
+import mython.quant
+
+def to5(filename):
+    """Convert a CSV file to an HDF5 file. It is fairly intelligent about dates and strings.
+    Dates will be expressed as floats of the form YYYYMMDD
+
+    Pandas has trouble distinguishing between dates and strings.
+    """
+    
+    df = mython.quant.load_to_pandas(filename)
+    try:
+        h5 =h5py.File('/home/mcarter/.fortran/' + filename + '.h5')
+        h5.attrs['Created'] = str(datetime.datetime.now())
+        for cname in df.columns:
+            arr = np.array(df[cname])
+            atts = None
+            #if df[cname].dtype == datetime.date:
+            #    def d2f(x): [y, m, d] = map(int, str(x).split('-')) ; return y *10000 + m * 100 + d
+            #    arr = np.array([d2f(x) for x in arr], dtype = 'float')
+            #    atts = { 'NB': "In float form for octave compatability", 'Format' : 'YYYYMMDD' }
+            if arr.dtype == 'O': # probably a string
+                mlen = max([len(str(a)) for a in arr])
+                arr = arr.astype(dtype = "S" + str(mlen))
+
+            dset = h5.create_dataset(cname, data = arr, compression='gzip')
+            if atts:
+                for k, v in atts.items(): dset.attrs[k] = v
+    finally:    
+        h5.close()
+
+if __name__ == "__main__":
+    p = argparse.ArgumentParser()
+    p.add_argument('--debug', action = "store_true", help = "Print the arguments")
+    p.add_argument('--to5', action = "store", help = "Convert csv file to HDF5. File must be in ~/.fortran directory")
+
+    args = p.parse_args()
+    if args.debug: print(args)
+    if args.to5: to5(args.to5)
